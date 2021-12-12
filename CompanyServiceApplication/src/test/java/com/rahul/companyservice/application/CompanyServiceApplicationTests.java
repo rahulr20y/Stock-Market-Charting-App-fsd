5ab0c7be5f8aba132c55fb6d884f02d6ffd4bd55
Rahul Yadav <80162537+rahulr20y@users.noreply.github.com>
2021-12-12 23:49:19 +0530

    Add files via upload

---
+++ CompanyServiceApplication/src/test/java/com/rahul/companyservice/application/CompanyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.companyservice.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
