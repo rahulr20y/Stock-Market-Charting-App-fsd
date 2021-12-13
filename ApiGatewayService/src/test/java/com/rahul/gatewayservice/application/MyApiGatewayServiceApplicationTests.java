e6f5442487b35f45d3078d27eeb4b6d49bc2bb56
Rahul Yadav <80162537+rahulr20y@users.noreply.github.com>
2021-12-13 10:24:13 +0530

    Add files via upload

---
+++ ApiGatewayService/src/test/java/com/rahul/gatewayservice/application/MyApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.gatewayservice.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
